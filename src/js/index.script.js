077fce345b006fbd7a8798915fb5c2859bbc1e2b
ikerdev <106914240+ikermoon3@users.noreply.github.com>
2022-07-19 23:10:50 +0200

    Add files via upload

---
+++ src/js/index.script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal()
+
+ScrollReveal().reveal('.header', {
+    duration: 1500
+})
+
+ScrollReveal().reveal('.titleCentered', { distance: '100px', duration: 2500 });
+
+ScrollReveal().reveal('.textTitleCentered', { distance: '100px', duration: 4500 });
+
+ScrollReveal().reveal('.videos', { distance: '100px', duration: 4500, delay: 1200 });
